5619404df38189ce5528ca43ca56f3111c414847
ghkdwjdrk123 <ghkdwjdrk123@naver.com>
2024-10-13 23:34:26 +0900

    7번 Vue Router 추가까지 진행

---
+++ src/main/java/org/gallery/backend/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package org.gallery.backend.repository;
+
+import org.gallery.backend.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+}
